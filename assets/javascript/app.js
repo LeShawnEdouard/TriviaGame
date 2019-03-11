5a251cd63fb04f1af285f56627398ce7cc59503e
LeShawn Edouard <leshawnedouard92@gmail.com>
2019-03-10 21:57:01 -0400

    Added questions/answeres array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,41 @@
-var correctAnswers = 0;
-var incorrectAnswers = 0;
-var unanswered = 0
 
+// Question and answer array //
+var questions = 
+[
+    {
+    phrase: "Sugar, spice and everything nice",
+    choices: ["Johnny Bravo", "I Am Weasel", "Powerpuff Girls", "Cow and Chicken"],
+    answer: 2
+    },
+    {
+    phrase2: "Long ago in a distant land, I, Aku, the shapeshifting master of darkness...",
+    choices: ["Courage the Cowardly Dog", "Ed, Edd n Eddy", "Dexter's Laboratory", "Samurai Jack"],
+    answer: 3
+    },
+    {
+    phrase3: "Supid dog, you make me look bad",
+    choices: ["Total Drama", "Courage the Cowardly Dog", "Teen Titans", "The Flintstones"],
+    answer: 1
+    }
+];
+
+
+// Function to set the game //
+function setGame()  {
+  var correctAnswers = 0;
+  var incorrectAnswers = 0;
+  var unanswered = 0
+}
+
+
+
+// Function to start the game //
+$(".btn").on("click", function () {
+  // Function call to set the game //
+  setGame();
+
+
+
+
+
+});

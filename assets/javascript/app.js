470bc82cd9e2f350ce83324a79e665f2f54b6845
LeShawn Edouard <leshawnedouard92@gmail.com>
2019-03-12 23:38:17 -0400

    Adding for loop to randomize questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,17 +3,17 @@
 var questions = 
 [
     {
-    phrase1: "Sugar, spice and everything nice",
+    phrase: "Sugar, spice and everything nice",
     choices: ["Johnny Bravo", "I Am Weasel", "Powerpuff Girls", "Cow and Chicken"],
     answer: 2
     },
     {
-    phrase2: "Long ago in a distant land, I, Aku, the shapeshifting master of darkness...",
+    phrase: "Long ago in a distant land, I, Aku, the shapeshifting master of darkness...",
     choices: ["Courage the Cowardly Dog", "Ed, Edd n Eddy", "Dexter's Laboratory", "Samurai Jack"],
     answer: 3
     },
     {
-    phrase3: "Supid dog, you make me look bad",
+    phrase: "Supid dog, you make me look bad",
     choices: ["Total Drama", "Courage the Cowardly Dog", "Teen Titans", "The Flintstones"],
     answer: 1
     }
@@ -29,15 +29,22 @@ function setGame()  {
 
 // Function to start the game //
 $(".btn").on("click", function () {
-  // Function call to set the game //
-  setGame();
-  console.log(setGame);
-
-  $(this).hide();
 
   for (var i = 0; i < questions.length; i++) {
-    
+  var display = $("#question").text(questions.phrase[i]);
+
+  // ($(this).text(question.phrase),
+  console.log(questions);
   }
+
+  
+  // $("#questions").append(questions);
+
+
+  // setGame();
+  // console.log(setGame);
+
+
 
 
 
